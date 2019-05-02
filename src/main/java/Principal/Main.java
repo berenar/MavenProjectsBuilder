8f043c09d5518ead67b0533b54641a328d3b5e81
berenar <bernatpericasserra97@gmail.com>
2019-05-02 10:37:17 +0200

    intent de GridBagLayout

--- src/main/java/Principal/Main.java
+++ src/main/java/Principal/Main.java
@@ -7,14 +7,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Main extends JFrame {
+public class Main extends JFrame{
 
     private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
     ArrayList<Project_pan> selected_projs = new ArrayList<Project_pan>();
 
     //initial sizes
-    private int panell_width = (screenSize.width / 3);
+    private int panell_width = 700;
     private int panell_height = 100;
 
     public Main() {
@@ -23,34 +23,46 @@ public class Main extends JFrame {
 
     public void initUI() {
 
-        Container contentPane = getContentPane();
-        contentPane.setLayout(null);
+        JFrame jf = new JFrame();
+        jf.setLayout(new GridBagLayout());
+
+        //Container contentPane = jf.getContentPane();
+        //contentPane.setLayout(null);
 
         //TITLE
         JLabel titol_app = new JLabel("Select projects to be compiled");
         titol_app.setBounds(((panell_width / 2) - 100), 0, 200, 50);
-        contentPane.add(titol_app);
+        jf.add(titol_app);
 
         //PROJECTS
-        nou_project_pan(contentPane);
+        nou_project_pan(jf.getContentPane());
+        nou_project_pan(jf.getContentPane());
 
         //WINDOW CONFIGURATION
-        setSize(panell_width, panell_height);
-        setLocationRelativeTo(null);//null: al centre de la pantalla
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setTitle("mvnCompiler 1.0");
+        jf.setSize(panell_width, panell_height);
+        jf.setLocationRelativeTo(null);//null: al centre de la pantalla
+        //jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jf.setTitle("mvnCompiler 1.0");
         try {
-            setIconImage(ImageIO.read(new File(System.getProperty("user.dir") + "/media/mvn_logo.png")));
+            jf.setIconImage(ImageIO.read(new File(System.getProperty("user.dir") + "/media/mvn_logo.png")));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        jf.pack();
+        jf.setVisible(true);
     }
 
     public void nou_project_pan(Container contentPane) {
+        GridBagConstraints constraints = new GridBagConstraints();
         Project_pan proj = new Project_pan();
         selected_projs.add(proj);
-        proj.configurar_project_pan(selected_projs.size());
-        proj.afegir_project_pan(contentPane);
+
+        //proj.configurar_project_pan(selected_projs.size());
+        constraints.gridx=0;
+        constraints.gridy=0;
+        constraints.gridwidth=2;
+        constraints.gridheight=4;
+        proj.afegir_project_pan(contentPane,constraints);
         recalcular_window(proj);
     }
 
@@ -59,8 +71,8 @@ public class Main extends JFrame {
     }
 
     public static void main(String[] args) {
-        Main ex = new Main();
-        ex.setVisible(true);
+        new Main();
+        //ex.setVisible(true);
     }
 
 }
--- src/main/java/Principal/Project_pan.java
+++ src/main/java/Principal/Project_pan.java
@@ -29,21 +29,22 @@ public class Project_pan extends JPanel {
         this.jl_order = new JLabel("a");
         this.jl_order_border = BorderFactory.createLineBorder(Color.GRAY, 1);
         this.jl_path = new JLabel(" ...");
+
         this.jb_fc = fc.go;
         this.fc_border = BorderFactory.createLineBorder(Color.GRAY, 1);
         this.triat = false;
     }
 
     public void configurar_project_pan(int n) {
-        jl_order.setBounds(x_inicial, y_inicial * n, jl_order_size, jl_order_size);
+/*        jl_order.setBounds(x_inicial, y_inicial * n, jl_order_size, jl_order_size);
         jl_order.setBorder(jl_order_border);
         jl_path.setBounds(x_inicial + jl_order_size, y_inicial * n, jl_path_width, jl_path_height);
         jl_path.setBorder(fc_border);
-        jb_fc.setBounds(x_inicial + jl_path_width + 40, y_inicial * n, jb_fc_width, jb_fc_height);
+        jb_fc.setBounds(x_inicial + jl_path_width + 40, y_inicial * n, jb_fc_width, jb_fc_height);*/
     }
 
-    public void afegir_project_pan(Container contentPane) {
-        contentPane.add(jl_path);
+    public void afegir_project_pan(Container contentPane, GridBagConstraints constraints) {
+        contentPane.add(jl_path,constraints);
         contentPane.add(jb_fc);
     }
 
